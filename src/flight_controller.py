47a0af1dde263255bb3e8a212543c85d337467db
alexxterms <ali.khn93599@gmail.com>
2025-04-04 01:55:13 +0530

    .

--- src/flight_controller.py
+++ src/flight_controller.py
@@ -1,42 +1,25 @@
 import time
 import struct
-import serial
 from yamspy import MSPy
 
 class FlightController:
-    def __init__(self, serial_port, baudrate=115200):
-        try:
-            self.serial_port = serial_port
-            self.baudrate = baudrate
+    def __init__(self, serial_port="/dev/ttyACM1", baudrate=115200):
+        self.board = MSPy(device=serial_port, loglevel='DEBUG', baudrate=baudrate)
+        self.imu_data = {
+            "accelerometer": [0, 0, 0],
+            "gyroscope": [0, 0, 0],
+            "magnetometer": [0, 0, 0]
+        }
 
-            
-
-            # Initialize MSPy
-            self.board = MSPy(device=serial_port, loglevel='DEBUG', baudrate=baudrate)
-            
-            self.imu_data = {
-                "accelerometer": [0, 0, 0],
-                "gyroscope": [0, 0, 0],
-                "magnetometer": [0, 0, 0]
-            }
-        except serial.SerialException as e:
-            print(f"❌ ERROR: Could not open serial port {serial_port}. Make sure it's correct and not in use.")
-            raise e
-    
-    def get_imu_data(self):
+    def read_imu(self):
         """Requests and reads raw IMU data from the flight controller."""
         if self.board.send_RAW_msg(MSPy.MSPCodes['MSP_RAW_IMU']):
             data_length = 18
             msg = self.board.receive_raw_msg(size=(6+data_length))[5:]
             converted_msg = struct.unpack('<%dh' % (data_length // 2), msg[:-1])
 
-            # Scale Accelerometer values (convert to G-force)
             acc_x, acc_y, acc_z = [val / 512.0 for val in converted_msg[:3]]
-            
-            # Scale Gyroscope values (convert to degrees/sec)
             gyro_x, gyro_y, gyro_z = [val * (4.0 / 16.4) for val in converted_msg[3:6]]
-            
-            # Magnetometer raw values
             mag_x, mag_y, mag_z = converted_msg[6:9]
             
             self.imu_data = {
@@ -45,7 +28,7 @@ class FlightController:
                 "magnetometer": [mag_x, mag_y, mag_z]
             }
         return self.imu_data
-    
+
     def send_rc_command(self, channels):
         """Sends RC commands to the flight controller."""
         if len(channels) != 8:
@@ -54,7 +37,5 @@ class FlightController:
     
     def close(self):
         """Closes the serial connection."""
-        if self.board.ser.is_open:
-            self.board.ser.close()
-            print("🔌 Serial port closed.")
+        self.board.ser.close()
 
--- src/imu_reader.py
+++ src/imu_reader.py
@@ -1,52 +1,6 @@
-import time
-import math
 from flight_controller import FlightController
 
-class IMUReader:
-    def __init__(self, fc: FlightController):
-       
-        self.fc = fc
-
-    def read_imu(self):
-        imu_data = self.fc.get_imu_data()
-        if imu_data:
-            accel, gyro, mag = imu_data["accelerometer"], imu_data["gyroscope"], imu_data["magnetometer"]
-            
-            # Compute Roll & Pitch angles from accelerometer
-            roll_angle = math.degrees(math.atan2(accel[1], accel[2]))
-            pitch_angle = math.degrees(math.atan2(-accel[0], math.sqrt(accel[1]**2 + accel[2]**2)))
-            
-            # Determine orientation based on angles
-            orientation = []
-
-            if pitch_angle > 10:
-                orientation.append("Nose Up")
-            elif pitch_angle < -10:
-                orientation.append("Nose Down")
-
-            if roll_angle > 10:
-                orientation.append("Right Wing Down")
-            elif roll_angle < -10:
-                orientation.append("Left Wing Down")
-
-            if gyro[2] > 5:
-                orientation.append("Turning Right")
-            elif gyro[2] < -5:
-                orientation.append("Turning Left")
-
-            if accel[2] > 1.1:
-                orientation.append("Climbing")
-            elif accel[2] < 0.9:
-                orientation.append("Descending")
-
-            if not orientation:
-                orientation.append("Level Flight")
-            
-            print(f"\U0001F4E1 Processed IMU Data:")
-            print(f"Accelerometer (G): {accel}")
-            print(f"Gyroscope (deg/sec): {gyro}")
-            print(f"Magnetometer: {mag}")
-            print(f"\U0001F4E1 Orientation: {', '.join(orientation)}")
-            print("-" * 50)
-        return imu_data
+fc = FlightController("/dev/ttyACM1")  
+imu_data = fc.read_imu()  
+print(imu_data)  
 
