865952280a4698d0ad4bf59573b7900b8e5fe01c
alexxterms <ali.khn93599@gmail.com>
2025-04-03 18:32:53 +0530

    processed IMU DATA

--- src/test_imu.py
+++ src/test_imu.py
@@ -6,6 +6,8 @@ with MSPy(device=serial_port, loglevel='DEBUG', baudrate=115200) as board:
     board.fast_read_imu()
 
     print("📡 IMU Data:")
-    print(f"Accelerometer: {board.SENSOR_DATA['accelerometer']}")
-    print(f"Gyroscope: {board.SENSOR_DATA['gyroscope']}")
-    print(f"Magnetometer: {board.SENSOR_DATA['magnetometer']}")
+    print("📡 Processed IMU Data:")
+    print(f"Accelerometer (G): {[x / 512 for x in board.SENSOR_DATA['accelerometer']]}")
+    print(f"Gyroscope (deg/sec): {[x * (4 / 16.4) for x in board.SENSOR_DATA['gyroscope']]}")
+    print(f"Magnetometer: {board.SENSOR_DATA['magnetometer']}")  # Usually raw
+
