112735ccd876b59dda79662bdc40fc4b1b5b7f68
alexxterms <ali.khn93599@gmail.com>
2025-04-03 18:09:55 +0530

    .

--- src/test_imu.py
+++ src/test_imu.py
@@ -1,12 +1,10 @@
 from yamspy import MSPy
 
-serial_port = "/dev/ttyACM1"
+serial_port = "/dev/ttyACM0"
 
 with MSPy(device=serial_port, loglevel='DEBUG', baudrate=115200) as board:
-    if board.send_RAW_msg(MSPy.MSPCodes['MSP_RAW_IMU'], data=[]):
+    if board.send_RAW_msg(MSPy.MSPCodes['MSP_ATTITUDE'], data=[]):
         dataHandler = board.receive_msg()
         board.process_recv_data(dataHandler)
 
-        print("📡 Raw IMU Response:", dataHandler)  # Print the entire response
-        if 'dataView' in dataHandler:
-            print("IMU Data View:", dataHandler['dataView'])  # Print raw IMU values
+        print("📡 Attitude Response:", dataHandler)
