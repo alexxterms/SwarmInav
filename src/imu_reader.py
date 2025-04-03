e5143aeab671a62df3ac1a7916d7309e3a375081
alexxterms <ali.khn93599@gmail.com>
2025-04-03 16:38:31 +0530

    fix read_imu

--- src/imu_reader.py
+++ src/imu_reader.py
@@ -9,9 +9,9 @@ def read_imu(board):
     board.send_RAW_msg(MSPy.MSPCodes['MSP_RAW_IMU'], data=[])
 
     response = board.receive_msg()
-    if response and response.cmd == MSPy.MSPCodes['MSP_RAW_IMU']:
-        acc_x, acc_y, acc_z = response.data[:3]  # Extract accelerometer values
-        gyro_x, gyro_y, gyro_z = response.data[3:6]  # Extract gyroscope values
+    if response and response.get('cmd') == MSPy.MSPCodes['MSP_RAW_IMU']:
+        acc_x, acc_y, acc_z = response['data'][:3]  # Extract accelerometer values
+        gyro_x, gyro_y, gyro_z = response['data'][3:6]  # Extract gyroscope values
         return acc_x, acc_y, acc_z, gyro_x, gyro_y, gyro_z
     
     return None  # Return None if no data is received
