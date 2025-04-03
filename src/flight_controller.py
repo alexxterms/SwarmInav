cf8771a611be071266f7457a4e50378fd13ca335
alexxterms <ali.khn93599@gmail.com>
2025-04-04 04:15:05 +0530

    changed serial port

--- src/flight_controller.py
+++ src/flight_controller.py
@@ -3,7 +3,7 @@ import struct
 from yamspy import MSPy
 
 # Setup
-serial_port = "/dev/ttyACM0"
+serial_port = "/dev/ttyACM2"
 baudrate = 115200
 
 with MSPy(device=serial_port, baudrate=baudrate) as board:
