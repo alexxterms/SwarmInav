93156332e585affadd8fa55aab5bbe9b2aa8a49a
alexxterms <ali.khn93599@gmail.com>
2025-04-03 18:13:01 +0530

    .

--- src/test_imu.py
+++ src/test_imu.py
@@ -3,8 +3,9 @@ from yamspy import MSPy
 serial_port = "/dev/ttyACM1"
 
 with MSPy(device=serial_port, loglevel='DEBUG', baudrate=115200) as board:
-    if board.send_RAW_msg(MSPy.MSPCodes['MSP_ATTITUDE'], data=[]):
+    if board.send_RAW_msg(MSPy.MSPCodes['MSP_ALTITUDE'], data=[]):
         dataHandler = board.receive_msg()
         board.process_recv_data(dataHandler)
 
         print("📡 Attitude Response:", dataHandler)
+        print(board.SENSOR_DATA['altitude'])
